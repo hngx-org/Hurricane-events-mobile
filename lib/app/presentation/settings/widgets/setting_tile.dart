37001d79a65ee31e321d2e7549596f035dee3673
Samuel Ademujimi <samshotmedia01@gmail.com>
2023-09-20 07:48:27 +0100

    fix: removed unused imports

--- lib/app/presentation/settings/widgets/setting_tile.dart
+++ lib/app/presentation/settings/widgets/setting_tile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 import 'package:hurricane_events/component/utils/extensions.dart';
 
