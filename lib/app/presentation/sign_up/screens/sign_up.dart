a91715778f34e46e418142ab5e19c87882bb7d76
ooluseye16 <obisesanoluseye16@gmail.com>
2023-09-20 14:07:00 +0100

    feat: done adding sign up screen

--- lib/app/presentation/sign_up/screens/sign_up.dart
+++ lib/app/presentation/sign_up/screens/sign_up.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hurricane_events/app/presentation/home/home.dart';
+import 'package:hurricane_events/app/presentation/sign_up/widgets/google_button.dart';
+import 'package:hurricane_events/app/router/base_navigator.dart';
+import 'package:hurricane_events/component/constants/images.dart';
+import 'package:hurricane_events/component/utils/extensions.dart';
 
 class SignUpScreen extends StatelessWidget {
   static const routeName = "sign_up";
@@ -7,7 +12,43 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                "Welcome on board!",
+                style: TextStyle(
+                  fontSize: 64,
+                  fontFamily: 'Work Sans',
+                  fontWeight: FontWeight.w700,
+                  height: 0,
+                ),
+              ),
+              96.height,
+              Image.asset(
+                AppImages.imgWelcome,
+                height: 234,
+                width: 234,
+              ),
+              96.height,
+              Text(
+                "Sign in or Create an account",
+                style: context.body1.copyWith(fontSize: 23),
+              ),
+              32.height,
+              GoogleButton(
+                onTap: () {
+                  BaseNavigator.pushNamedAndclear(HomeScreen.routeName);
+                },
+              )
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
